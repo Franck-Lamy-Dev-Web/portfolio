5a87c28e10b6f8edac726cf49f4e6f4ab46cf30d
Franck Lamy <francklamymagie@gmail.com>
2024-08-26 22:55:54 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let darkMode = false;
+
+function changeDarkMode() {
+  if (darkMode) {
+    darkMode = false;
+    document.documentElement.style.setProperty("--text-color", "#1E1E2F");
+    document.documentElement.style.setProperty("--background-color", "#efe7e5");
+    document.getElementById("dark-light-mode").innerHTML = "Dark mode";
+  } else {
+    darkMode = true;
+    document.documentElement.style.setProperty("--text-color", "white");
+    document.documentElement.style.setProperty("--background-color", "#1E1E2F");
+    document.getElementById("dark-light-mode").innerHTML = "Light mode";
+  }
+}
