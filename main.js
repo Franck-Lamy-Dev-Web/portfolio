834c742b5915631b9f999271a76ffc7d55f47027
Franck Lamy <francklamymagie@gmail.com>
2024-08-28 12:09:39 +0200

    formulaire

--- main.js
+++ main.js
@@ -14,3 +14,29 @@ function changeDarkMode() {
   }
 }
 
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault(); // Empêche l'envoi normal du formulaire
+
+  const formData = new FormData(this);
+
+  fetch('http://localhost/Portefolio/submit_form.php', {
+      method: 'POST',
+      body: formData,
+  })
+  .then(response => response.json())
+  .then(data => {
+      if (data.message) {
+          // Remplace le formulaire par un message de succès
+          document.querySelector('.formulaire').innerHTML = '<p class="success-message">Demande envoyée avec succès!</p>';
+      } else if (data.error) {
+          // Affiche le message d'erreur
+          document.querySelector('.formulaire').innerHTML = `<p class="error-message">${data.error}</p>`;
+      }
+  })
+  .catch(error => {
+      console.error('Erreur:', error);
+      document.querySelector('.formulaire').innerHTML = '<p class="error-message">Une erreur est survenue lors de l\'envoi de votre demande.</p>';
+  });
+});
+
